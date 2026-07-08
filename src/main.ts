ef929d8ce5dc893811053d59d0ade3573255b3a6
mev <32826289+mevvern@users.noreply.github.com>
2026-07-08 04:07:26 -0500

    fixes it again for the third time and it really works this time i promise please just let it work i th

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { basicSetup } from "codemirror"
 import { EditorView } from "@codemirror/view"
 import { javascript } from "@codemirror/lang-javascript"
 
-const wawa: string = "baba"
-
 const view = new EditorView({
 	doc: "Start document",
 	parent: document.body,
